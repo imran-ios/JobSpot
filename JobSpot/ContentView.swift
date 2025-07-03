f6a6e5bd38c6520e36695cb85013117e1407a792
Md Imran Ali test <mdimranali94@gmail.com>
2025-07-03 21:56:18 +0530

    Initial Commit

---
+++ JobSpot/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JobSpot
+//
+//  Created by Md Imran Ali on 03/07/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ JobSpot/JobSpotApp.swift
@@ -0,0 +1,17 @@
+//
+//  JobSpotApp.swift
+//  JobSpot
+//
+//  Created by Md Imran Ali on 03/07/25.
+//
+
+import SwiftUI
+
+@main
+struct JobSpotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
